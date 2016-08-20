24aa1b0ffde7d1aaa1b506d21dac1b50d1efb6ac
BlindingDark <844620174@qq.com>
2016-08-21 00:30:35 +0800

    修复了对中英之外翻译的错误（进行url encode）

--- app/src/main/java/com/blindingdark/geektrans/trans/Youdao.java
+++ app/src/main/java/com/blindingdark/geektrans/trans/Youdao.java
@@ -7,6 +7,9 @@ import com.blindingdark.geektrans.GetTransTextActivity;
 import com.blindingdark.geektrans.api.TransReq;
 import com.blindingdark.geektrans.tools.PostAndGet;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * Created by BlindingDark on 2016/8/20 0020.
  */
@@ -14,7 +17,13 @@ public class Youdao implements TransReq {
     String query;
 
     public Youdao(String query) {
-        this.query = query;
+        try {
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
+            this.query = encodedQuery;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            this.query = query;
+        }
     }
 
     @Override
